75883b421d123c2fd6d666e46c50a405123a77fd
TridevSharma1 <tridevx8@gmail.com>
2025-10-31 18:24:58 +0530

    first commit

---
+++ learningL-1.py
@@ -0,0 +1,83 @@
+#print
+print("Tridev Sharma" ,  "Hello Everyone")
+print("Age 17")
+print("35+23")
+print(35+23)
+
+#variable=value
+name = "Tridev Sharm"
+age = 17
+price = 50.99
+print(name, age, price)
+print("My Name Is =" , name)
+print("my Age Is =",age)
+print("price =",price)
+
+#"""print sum""" artmatic opration
+a = 2
+b = 4
+sum = a+b
+sum1 = a*b
+sum3 = a**b #a^b
+sum4 = a % b #reminders
+print(sum , sum1 , sum3 , sum4)
+
+#relational opration
+
+a = 50 
+b = 20
+
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+#assignment oprator
+
+a = 30
+b = 20
+c = 30
+d = 40
+
+a += 10
+b -= 20
+c /= 3
+d *= 2
+
+print(a , b , c , d)
+
+#logical oprators
+
+print(not False)
+print(not True)
+
+val1 = True
+val2 = False
+
+print("and oprator :" , val1 and val2)
+
+print("or oprator :" , val1 or val2)
+
+#type conversion
+
+a = 400
+b = 300.44
+
+sum = a+b #400.00 + 300.44 a + b
+
+#type casssting
+
+a,b = 200,"200"
+c = int(b)
+
+print(a+c)
+
+#input
+
+name = input("enter your name:", )
+age = int(input("enter your age",))
+No = int (input("Enter Mobile No",))
+print("WELCOME ", name , age , No)
+
