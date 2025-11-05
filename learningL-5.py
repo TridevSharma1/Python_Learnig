63f7fec697a3fcfb0e305c059e7d1cd9b37fe05c
TridevSharma1 <tridevx8@gmail.com>
2025-11-05 21:28:51 +0530

    fifth

---
+++ learningL-5.py
@@ -0,0 +1,26 @@
+count = 3
+while count <= 90 :
+    print('Tridev Sharma' , count )
+    count += 3 
+
+# n = int(input('Enter No -->'))
+# i = 1
+# while i <= 100 :
+#     print(n*i)
+#     i += 1
+
+
+num = [1,4,9,12,3,4,5,6,7,8]
+idx = 0
+while idx < len(num):
+    print(num[idx])
+    idx += 1
+
+num1 = (1,4,9,12,3,4,5,6,7,8)
+x = int(input('Number --> '))
+i = 0
+while i < len(num1):
+    if (num1[i] == x):
+      print('Found At Index -->',i)
+    i += 1
+
