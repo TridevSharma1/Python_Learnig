f359d6d6193dfae97bb4b020dd90f4497e15ceb9
TridevSharma1 <tridevx8@gmail.com>
2025-11-30 20:44:39 +0530

    Oprators

---
+++ Oprators.py
@@ -0,0 +1,56 @@
+
+# artmatic opration
+a = 2
+b = 4
+sum = a+b
+sum1 = a*b
+sum3 = a**b #a^b
+sum4 = a % b #reminders
+print(sum , sum1 , sum3 , sum4)
+
+#relational opration
+
+a = 50 
+b = 20
+
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+#assignment oprator
+
+a = 30
+b = 20
+c = 30
+d = 40
+
+a += 10
+b -= 20
+c /= 3
+d *= 2
+
+print(a , b , c , d)
+
+#logical oprators
+
+print(not False)
+print(not True)
+
+val1 = True
+val2 = False
+
+print("and oprator :" , val1 and val2)
+
+print("or oprator :" , val1 or val2)
+
+a = 10
+b = 20
+
+print(a>b)
+print(a<b)
+print(not(a>b))
+print(not(a<b and a<b))
+print(not(a<b or a<b))
