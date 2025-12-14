52044837d6e1e2006e91a927599e63d051264b50
TridevSharma1 <tridevx8@gmail.com>
2025-12-14 20:15:31 +0530

    Indexing

---
+++ Indexing.py
@@ -0,0 +1,22 @@
+name = "tridev Shrama"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+print(name[9])
+print(name[10])
+print(name[11])
+print(name[12])
+
+name1 = "Tridev"
+print(name1[-1])
+print(name1[-2]) 
+print(name1[-3])
+print(name1[-4])
+print(name1[-5])
+print(name1[-6])
