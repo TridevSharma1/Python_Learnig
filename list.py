e4503f6ca7adfe943664858a5a88911b14ccc3dc
TridevSharma1 <tridevx8@gmail.com>
2025-12-23 19:50:58 +0530

    List 3

--- list.py
+++ list.py
@@ -34,5 +34,14 @@ print(constructed_list) # Output: [1, 2, 3, 4]
 repeated_list = [7] * 5
 print(repeated_list) # Output: [7, 7, 7, 7, 7]
 
+# list with duplicates
+duplicates_list = [1, 2, 2, 3, 4, 4, 4]
+print(duplicates_list) # Output: [1, 2, 2, 3, 4, 4, 4]
 
+# list comprehension
+squared_list = [x**2 for x in range(6)]
+print(squared_list) # Output: [0, 1, 4, 9, 16, 25]
 
+# with conditional logic
+even_squares = [x**2 for x in range(10) if x % 2 == 0]
+print(even_squares) # Output: [0, 4, 16, 36, 64]
