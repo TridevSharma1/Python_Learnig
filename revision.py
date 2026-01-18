a4cbf180178804bb6e9bf8e3472e96ac797dbd65
TridevSharma1 <tridevx8@gmail.com>
2026-01-18 18:26:49 +0530

    Update

--- revision.py
+++ revision.py
@@ -1,29 +1,29 @@
 # for loop => 
 # 1. check condition => true 
 
-# # # 2. execute some lines of code 
+# 2. execute some lines of code 
 
-# # # 3. i ++
+# 3. i ++
 
-# # # for(int i = 0; i < 3 ; i++){       
-# # #    pritnf("Rudra");                                                 
-# # # }                                    
+# for(int i = 0; i < 3 ; i++){       
+#    pritnf("Rudra");                                                 
+# }                                    
 
-# # for i in range(0,5):
-# #     print("Rudra") 
+# for i in range(0,5):
+#     print("Rudra") 
 
-# # # i = inistialization
-# # # condition 
-# # for i in range(10): 
-# #     print(i)
-# # # 0 1 2 3 4 5 6 7 8 9
+#  i = inistialization
+#  condition 
+# for i in range(10): 
+#     print(i)
+#  0 1 2 3 4 5 6 7 8 9
 
-# # for i in range(1,11):
-# #     print(i)
-# # # 1 2 3 4 5 6 7 8 9 10
+# for i in range(1,11):
+#     print(i)
+# 1 2 3 4 5 6 7 8 9 10
 
-# # for i in range(0,11,2):
-# #     print(i)
+#  for i in range(0,11,2):
+#      print(i)
 
 # name = input("Enter your name : ")
 # age = int(input("Enter your age : "))
@@ -39,11 +39,11 @@
 # ending point => 210 ( excluded)
 # 20,40,60,80,100,120,140,160,180,200
 
-i = 0
+# i = 0
 
-while(i<10):
-     print(i)
-     i += 2
+# while(i<10):
+#      print(i)
+#      i += 2
 
 # i +=1 or i = i + 1 = same
 # i => Loop variable
