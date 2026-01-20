979c882cf0832c4d7903c974c925eb4f8f1fee49
TridevSharma1 <tridevx8@gmail.com>
2026-01-20 20:57:00 +0530

    revision

--- revision.py
+++ revision.py
@@ -51,23 +51,26 @@
 # increment => i += 2
 
 # break , continue , pass
-for i in range(1,11):
-    if i == 5:
-        break
-    print(i)
+
+# for i in range(1,11):
+#     if i == 5:
+#         break
+#     print(i)
 # 1 2 3 4
-for i in range(1,11):
-    if i == 5:
-        continue
-    print(i)
+
+# for i in range(1,11):
+#     if i == 5:
+#         continue
+#     print(i)
 # 1 2 3 4 6 7 8 9 10
-for i in range(1,11):
-    if i == 5:
-        pass
-    print(i)
+
+# for i in range(1,11):
+#     if i == 5:
+#         pass
+#     print(i)
 # 1 2 3 4 5 6 7 8 9 10
 
-
+# 
 
 
 
