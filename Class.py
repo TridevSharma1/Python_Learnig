a51cd16c27c3729e59b3bc1f3c81c84d7802c961
TridevSharma1 <tridevx8@gmail.com>
2026-03-27 21:41:18 +0530

    Py

--- Class.py
+++ Class.py
@@ -1,3 +1,15 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 print("Hello",name,"You are",age,"years old.")
+# class
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print("Hello", self.name, "You are", self.age, "years old.")
+# create an object of the class
+person1 = Person(name, age)
+# call the method
+person1.greet()
